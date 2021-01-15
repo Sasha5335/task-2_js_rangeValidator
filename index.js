48b0131aba6773eab6a8437923e515a684faf0e4
Aleksandr <mironov5335@gmail.com>
2021-01-15 19:42:03 +0200

    Tab size

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 'use strict';
 
 class RangeValidator {
-	/**
-	 * @param {number} from 
-	 * @param {number} to 
-	 */
+   /**
+    * @param {number} from 
+    * @param {number} to 
+    */
 
-	constructor(from, to) {
-		this.from = from;
-		this.to = to;
-	}
+   constructor(from, to) {
+      this.from = from;
+      this.to = to;
+   }
 
-	set from(newFrom) {
-		if (typeof newFrom !== 'number') {
-			throw new TypeError('Insert the number');
-		}
-		if (newFrom < 0) {
-			throw new RangeError();
-		}
-		this._from = newFrom;
-	}
-	get from() {
-		return this._from;
-	}
+   set from(newFrom) {
+      if (typeof newFrom !== 'number') {
+         throw new TypeError('Insert the number');
+      }
+      if (newFrom < 0) {
+         throw new RangeError();
+      }
+      this._from = newFrom;
+   }
+   get from() {
+      return this._from;
+   }
 
-	set to(newTo) {
-		if (typeof newTo !== 'number') {
-			throw new TypeError('Insert the number!');
-		}
-		if (this._from > newTo) {
-			throw new RangeError();
-		}
-		this._to = newTo;
-	}
-	get to() {
-		return this._to;
-	}
+   set to(newTo) {
+      if (typeof newTo !== 'number') {
+         throw new TypeError('Insert the number!');
+      }
+      if (this._from > newTo) {
+         throw new RangeError();
+      }
+      this._to = newTo;
+   }
+   get to() {
+      return this._to;
+   }
 
-	get range() {
-		return [this._from, this._to];
-	}
+   get range() {
+      return [this._from, this._to];
+   }
 
-	validate(num) {
-		if (num >= this.from && num <= this.to) {
-			return num;
-		}
-		throw new TypeError('Number is out of range');
-	}
+   validate(num) {
+      if (num >= this.from && num <= this.to) {
+         return num;
+      }
+      throw new TypeError('Number is out of range!');
+   }
 }
-
+``
 const rangeValidator = new RangeValidator(18, 22);
